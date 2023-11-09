4be72e166e0bc3477f28a9dc24803e4a359dbc32
Vikash Burman <vikasburman37@gmail.com>
2023-11-09 12:02:49 +0530

    basic todo list added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import fTodoList from "./fTodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () =>{
+  const [inputList,setInputList] = useState("");
+  const[items,setItems] = useState([]);
+
+  const inputEvents =(event)=>{
+    setInputList(event.target.value);
+  }
+
+  const listOfItems = () =>{
+    setItems((oldItem)=>{
+      return [...oldItem,inputList];
+    })
+    setInputList("");
+  }
+  const deleteItems =(id)=>{
+    console.log(id);
+    setItems((oldItem)=>{
+      return oldItem.filter((arrElem,index)=>{
+          return index !== id;
+      })
+    })
+  }
+  return(
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br/>
+          <h1>To Do List</h1>
+          <br/>
+          <input type="text" placeholder="Add an Item" onChange={inputEvents} value={inputList}/>
+          <button onClick={listOfItems}>+</button>
+          <ol>
+            {/* <li>{inputList}</li> */}
+            {items.map((itemValue,ind)=>{
+              {/* return <fTodoList text = {itemValue}/>; */}
+              return (<>
+              <div className="todo-item-container">
+                <i class="fa-solid fa-trash" onClick={()=>deleteItems(ind)}></i>
+                <li>{itemValue}</li>
+                </div>
+              </>);
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
   );
 }
-
 export default App;
